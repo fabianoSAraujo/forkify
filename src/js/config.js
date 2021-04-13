6c71ef1984589611b4d21408ee015de0ac554750
fabianoSAraujo <araujo.s.fabiano@gmail.com>
2021-04-13 17:04:05 +0100

    Final project

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '7de75407-ea7e-4d4a-9867-3b7f17102eeb';
+export const MODAL_CLOSE_SEC = 2.5;
